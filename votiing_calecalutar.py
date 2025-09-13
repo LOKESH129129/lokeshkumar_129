27d7f64505deff715115b637e0c76ba34ec1840f
LOKESH129129 <lokeshz.mk@gmail.com>
2025-09-13 16:26:52 +0530

    Create votiing_calecalutar.py

---
+++ votiing_calecalutar.py
@@ -0,0 +1,10 @@
+name=input("PLease enter Your Sweet name")
+age=int(input("Please enter your age in numbers")
+if(age>=18 and age<50):
+  print("You ",age,"are elidgible for votting")
+elif(age>=50):
+  print("You ",age,"are senier zitizons")
+elif(age<18):
+  print("you",age,"are child wait some days for votting,.........")
+else:
+  print("inavalid value please enter the correct age")
